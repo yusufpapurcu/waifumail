6b48930b700b0fca0416030c3b51dcb3bdc9d583
Yusuf Papurcu <yusufturhanp@gmail.com>
2023-10-07 10:54:38 +0100

    init

---
+++ src/main/java/com/yusufpapurcu/waifumail/Greet.java
@@ -0,0 +1,15 @@
+package com.yusufpapurcu.waifumail;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Greet {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ src/test/java/com/yusufpapurcu/waifumail/WaifumailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufpapurcu.waifumail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaifumailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
